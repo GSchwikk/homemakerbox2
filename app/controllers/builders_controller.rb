fb1efcef275473be574317dc1f2c8c0959872386
Gschwikk@gmail.com <GSchwikk>
2015-09-15 18:11:57 +0200

    Update New Review form with clickable star ratings

--- app/controllers/builders_controller.rb
+++ app/controllers/builders_controller.rb
@@ -10,7 +10,12 @@ class BuildersController < ApplicationController
   # GET /builders/1
   # GET /builders/1.json
   def show
-    @reviews = Review.where(builder_id: @builder.id)
+    @reviews = Review.where(builder_id: @builder.id).order("created_at DESC")
+    if @reviews.blank?
+     @avg_rating = 0
+    else
+      @avg_rating = @reviews.average(:rating).round(2)
+    end
   end
 
   # GET /builders/new
