df0bc0e46ff2a0e21b01e750d3e0501ae08245f0
Gschwikk@gmail.com <GSchwikk>
2015-09-16 17:51:44 +0200

    Add data validations

--- app/models/builder.rb
+++ app/models/builder.rb
@@ -1,4 +1,16 @@
 class Builder < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	has_many :reviews
+
+	validates :name, :address, :phone, :website, :image, presence: true
+
+	validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    message: "must be in the format (123) 456-7890" }
+
+    validates :phone, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://" }
+
+    validates :address, format: { with: /\A\d+[^,]+,[^,]+, [^,]+, \d{4}\z/,
+    message: "must be in the format 350 Street Name, Suburb, City, 2060" }
+
 end
